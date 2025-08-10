eeb5ad7b9bcf2822e6aaea18745ccd923e92299c
Indhu priya Yanamala <indhu312006@gmail.com>
2025-08-10 23:15:30 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,5 @@
 print(" Welcome to Mad Libs: Secret Agent Adventure! ")
 print("Fill in the blanks to create your own secret agent story.\n")
-
 agent_name = input("Enter a secret agent name: ")
 adjective1 = input("Enter an adjective: ")
 gadget = input("Enter a fancy gadget: ")
@@ -13,13 +12,9 @@ weapon = input("Enter a type of weapon: ")
 story = f"""
 Agent {agent_name}, the world’s most {adjective1} spy, received a mysterious message.
 "Your mission, should you choose to accept it, involves the {gadget} prototype stolen in {city}."
-
 Without hesitation, Agent {agent_name} jumped into their {vehicle} and raced off to stop the evil mastermind, {evil_name}.
-
 But {evil_name} had a plan. Inside a hidden lair filled with lasers and {silly_action}, the villain waited, laughing like a {adjective2} hyena.
-
 With only a {weapon} in hand, Agent {agent_name} kicked open the door and shouted, “Justice is served — shaken, not stirred!”
-
 What happened next? Well… that’s classified.
 """
 print("\n Your Secret Agent Story ")
